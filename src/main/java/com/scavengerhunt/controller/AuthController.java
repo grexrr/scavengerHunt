a3cf89266f5ddfd9d8d4dcdc51aa4efcd699c87b
grexrr <wyatt_h.w@protonmail.com>
2025-04-29 19:32:39 +0100

    initializing db config

--- src/main/java/com/scavengerhunt/controller/AuthController.java
+++ src/main/java/com/scavengerhunt/controller/AuthController.java
@@ -1,7 +1,5 @@
 package com.scavengerhunt.controller;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
@@ -10,37 +8,50 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.scavengerhunt.model.User;
+import com.scavengerhunt.repository.UserRepository;
+
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    private static Map<String, User> userStore = new HashMap<>();
+    private final UserRepository userRepo;
 
+    public AuthController(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+    
     @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody RegisterRequest request){
-        if (userStore.containsKey(request.getUsername())) {
+    public ResponseEntity<String> register(@RequestBody IdentityRequest request){
+        if (userRepo.findByUsername(request.getUsername()).isPresent()) {
             return ResponseEntity.status(409).body("Username already exists.");
         }
-        String newPlayerID = UUID.randomUUID().toString();
-        User newUser = new User(request.getUsername(), request.getPassword(), newPlayerID);
-        userStore.put(request.getUsername(), newUser);
+
+        String playerId = UUID.randomUUID().toString();
+        User newUser = new User(request.getUsername(), request.getPassword(), playerId);
+        userRepo.save(newUser);
         return ResponseEntity.ok("Register success!");
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest request) {
-        User user = userStore.get(request.getUsername());
-        if (user == null || !user.getPassword().equals(request.getPassword())) {
-            return ResponseEntity.status(401).body("Invalid username or password.");
-        }
 
-        return ResponseEntity.ok(user.getPlayerId()); 
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody IdentityRequest request) {
+        return userRepo.findByUsername(request.getUsername())
+        .filter(user -> user.getPassword().equals(request.getPassword()))
+        .map(user -> ResponseEntity.ok(user.getPlayerId()))
+        .orElse(ResponseEntity.status(401).body("Invalid username or password."));
     }
 
+
+    @PostMapping("/logout")
+    public String logout() {
+        return "Logout";
+    }
+    
     //DTO
 
-    public static class RegisterRequest {
+    public static class IdentityRequest {
         private String username;
         private String password;
         // getters and setters
@@ -49,42 +60,5 @@ public class AuthController {
 
         public String getPassword() { return password; }
         public void setPassword(String password) { this.password = password; }
-    }
-
-    public static class User{
-        private String username;
-        private String password;
-        private String playerId;
-        
-        public User(String username, String password, String playerID){
-            this.setUsername(username);
-            this.setPassword(password);
-            this.setPlayerId(playerID);
-        }
-
-        public String getUsername() { return username; }
-        public void setUsername(String username) { this.username = username; }
-
-        public String getPassword() { return password; }
-        public void setPassword(String password) { this.password = password; }
-        
-        public String getPlayerId() { return playerId; }
-        public void setPlayerId(String playerId) { this.playerId = playerId; }
-
-    }
-    
-    public static class LoginRequest {
-        private String username;
-        private String password;
-        private String playerId;
-
-        public String getUsername() { return username; }
-        public void setUsername(String username) { this.username = username; }
-
-        public String getPassword() { return password; }
-        public void setPassword(String password) { this.password = password; }
-        
-        public String getPlayerId() { return playerId; }
-        public void setPlayerId(String playerId) { this.playerId = playerId; }
     }
 }

834f5c75f7e320c0a018b8ecddc9c3ad25929029
grexrr <wyatt_h.w@protonmail.com>
2025-11-05 19:49:57 +0000

    update

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import GamePage from './app/game';
+import SettingsPage from './app/settings';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen 
+          name="Game" 
+          component={GamePage} 
+        />
+        <Tab.Screen 
+          name="Settings" 
+          component={SettingsPage} 
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
